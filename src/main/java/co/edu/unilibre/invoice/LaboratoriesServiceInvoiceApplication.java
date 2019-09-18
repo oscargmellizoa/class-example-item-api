dcfd06af2d8cb356630803cb0abf47b33e76921e
Oscar MA <omellizo@gmail.com>
2019-09-18 11:36:28 -0500

    Ejemplo eureka

--- src/main/java/co/edu/unilibre/invoice/LaboratoriesServiceInvoiceApplication.java
+++ src/main/java/co/edu/unilibre/invoice/LaboratoriesServiceInvoiceApplication.java
@@ -2,10 +2,10 @@ package co.edu.unilibre.invoice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "product-service")
+@EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
 public class LaboratoriesServiceInvoiceApplication {
